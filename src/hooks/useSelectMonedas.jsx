a7021f14273faef522061a29aba9e22af1379263
Agus2020 <aagustin067@gmail.com>
2023-09-20 00:18:35 -0300

    Hook para los Select

---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+const Label = styled.label`
+    color:#FFF;
+`
+export default function useSelectMonedas(label,opciones){
+
+    const SelectMonedas = () =>(
+        <>
+            <Label>{label}</Label>
+            <select>
+                <option value=''>Seleccione</option>
+                {opciones.map(opcion=>(
+                    <option key={opcion.id} value={opcion.id}>
+                        {opcion.nombre}
+                    </option>
+                ))}
+            </select>
+        </>
+    )
+    return [SelectMonedas];
+}
